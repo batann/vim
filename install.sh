408afcf9dcc75be619a3af4bb7dba1d375bc5c25
batann <122584289+batann@users.noreply.github.com>
2024-10-14 21:43:13 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 sudo mv /home/batan/vim /home/batan/.vim
+if [[ ! -d /home/batan/.vim/templates ]]; then
+mkdir -p /home/batan/.vim/templates
+fi
+if [[ -d == "/home/batan/dot/templates" ]]; then 
+cp /home/batan/dot/templates/* /home/batan/.vim/templates/ 
+fi
